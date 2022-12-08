6a8ecfb64a9d4e755e8eb1c22eaf1a0b418dcf9a
djlee <ddongjunn@gmail.com>
2022-12-08 22:50:02 +0900

    init spring

---
+++ spring/src/main/java/mirror/opgg/djhc/DjhcApplication.java
@@ -0,0 +1,13 @@
+package mirror.opgg.djhc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjhcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjhcApplication.class, args);
+	}
+
+}
